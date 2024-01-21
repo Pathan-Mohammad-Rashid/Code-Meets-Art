6083faf43397183820c40a83d6ecbd04e03d9b34
Mohammad Rashid Pathan <141010397+Pathan-Mohammad-Rashid@users.noreply.github.com>
2024-01-22 02:16:59 +0530

    Create Pattern 4.py

---
+++ Pattern 4.py
@@ -0,0 +1,33 @@
+import turtle
+import random
+
+# Set up the screen
+screen = turtle.Screen()
+screen.setup(500, 500, startx=0, starty=0)
+
+# Create a turtle for the pattern
+pattern = turtle.Turtle()
+pattern.speed(100000000000)
+
+# Create a list of colors
+colors = ["blue","red", "black"]
+
+# Function to draw a square
+def draw_square(turtle, size):
+    for _ in range(4):
+        turtle.forward(size)
+        turtle.right(90)
+
+# Function to draw a triangle
+def draw_triangle(turtle, size):
+    for _ in range(3):
+        turtle.forward(size)
+        turtle.right(120)
+
+# Draw the pattern
+for i in range(10000):
+    # Change the color at random intervals
+    pattern.color(random.choice(colors))
+    # draw_square(pattern, i*2)
+    draw_triangle(pattern, i*2)
+    pattern.right(36)
